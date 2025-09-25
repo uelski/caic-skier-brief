47cafe6d24142167af33b09302c354200bba1549
Sam Vredenburgh <sam.vburgh@gmail.com>
2025-09-24 20:45:27 -0600

    add mantine and create component files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@mantine/core/styles.css';
+
+import { Box, Container, MantineProvider, Title, createTheme, type MantineColorsTuple } from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+  '#f1f4fe',
+  '#e4e6ed',
+  '#c8cad3',
+  '#a9adb9',
+  '#9094a3',
+  '#7f8496',
+  '#777c91',
+  '#63687c',
+  '#595e72',
+  '#4a5167'
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider theme={theme}>
+      <Box bg="myColor.9" mih="100dvh" w="100%">
+        <Container>
+            <Title c="myColor.0" ta="center">Avy Brief</Title>
+        </Container>
+      </Box>
+
+    </MantineProvider>
   )
 }
 
